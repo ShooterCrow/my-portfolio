0a8a098e92955cc9d6161eca3227025369b97c3f
Victor Onyekwere <cydollarsign1@gmail.com>
2025-03-15 02:05:24 +0100

    Update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"), // ✅ Ensure @ points to src
-      '.': path.resolve(__dirname, './'), 
     },
   },
 });
