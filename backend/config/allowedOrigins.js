7fe4e50ee13924d36ba8451c2ab6793dfcb142cb
Victor Onyekwere <cydollarsign1@gmail.com>
2025-03-15 09:33:47 +0100

    Update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,8 @@
 exports.allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
-  "https://my-portfolio-xsh6.onrender.com/",
-  "https://my-portfolio-xsh6.onrender.com",
-  "https://www.my-portfolio-xsh6.onrender.com/"
+  "https://my-portfolio-bfhc.onrender.com/",
+  "https://my-portfolio-bfhc.onrender.com",
+  "https://www.my-portfolio-bfhc.onrender.com/",
+  "my-portfolio-bfhc.onrender.com"
 ];
