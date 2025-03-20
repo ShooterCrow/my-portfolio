e9921f2d51ebee7a950d9e385a93e02a364f597a
Victor Onyekwere <cydollarsign1@gmail.com>
2025-03-20 14:57:42 +0100

    Update

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,3 +1,7 @@
 exports.allowedOrigins = [
-  "https://my-portfolio-bfhc.onrender.com"
+  "https://my-portfolio-bfhc.onrender.com",
+  "https://www.webdevlab.site/",
+  "https://www.webdevlab.site",
+  "http://www.webdevlab.site/",
+  "http://www.webdevlab.site"
 ];
